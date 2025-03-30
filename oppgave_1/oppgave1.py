f3af854e602ae2d84393919aee373ec3b8a179a9
anundjore <aanundjore@hotmail.com>
2025-03-30 21:36:03 +0200

    div

---
+++ oppgave_1/oppgave1.py
@@ -0,0 +1,13 @@
+#Her er en liste med tall
+nøstetListe = [[1, 3],[4, 21, -2], [3, 4]]
+
+
+#Her skal dere legge inn koden som skal finne det høyeste tallet i den nøstede listen.
+
+
+#Denne koden printer bare alle tallene i listene. 
+#Kan du modifisere koden til å bare printe det høyeste tallet?
+
+for liste in nøstetListe:
+    for tall in liste:
+        print(tall)
